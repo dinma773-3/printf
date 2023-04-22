987ac3011dee062526967d7587c06e140fc124db
God'sfavour <125495054+dinma773-3@users.noreply.github.com>
2023-04-22 16:44:59 +0200

    Update 0-print_character_string.c

--- 0-print_character_string.c
+++ 0-print_character_string.c
@@ -1,59 +1,94 @@
 #include "main.h"
-/**
- * print_all - prints anything
- * @format: list of types of arguments passed to the function
- *
- * Return: number of all character in the arguments
- */
-int _print(const char *format, ...)
-{
-	int count = 0, len = 0;
 
-	va_list args;
-	va_start(args, format);
-	for (;(*format); format++){
-		if (*format != '%')
-		{
-			write(1, format, 1);
-			count++;
-		}
-		else
-		{
-			format++;
-			switch (*format)
-			{
-				case 'c':
-					{
-						char c = va_arg(args, int);
-						_putchar(c);
-						count++;
-						break;
-					}
-				case 's':
-					{
-						char *s = va_arg(args, char*);
-						int i = 0;
-						for (; s[i]; i++)
-							len++;
-						i = 0;
-						while (i < len)
-						{
-							_putchar(s);
-							i++;
-						}
-						count += len;
-						break;
-					}
-				case '%':
-					{
-						write(1, "%", 1);
-						count++;
-						break;
-					}
-			}
-		}
-	}
-	va_end(args);
-	return (count);
+/**                                                                                                                                                  
+ * print_buffer - Prints the contents of the buffer if it exist                                                                                      
+ * @buffer: Array of chars                                                                                                                           
+ * @buff_ind: Index at which to add next char, represents the length.                                                                                
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+  if (*buff_ind > 0)
+    write(1, &buffer[0], *buff_ind);
+
+  *buff_ind = 0;
 }
 
+/**                                                                                                                                                  
+ * _printf - Printf function                                                                                                                         
+ * @format: format.                                                                                                                                  
+ * Return: Printed chars.                                                                                                                            
+ */
+int _printf(const char *format, ...)
+{
+  va_list list;
+  int print_char = 0, buff_ind = 0, i;
+  char buffer[BUFF_SIZE];
+  char *string;
+  unsigned int j;
+
+  va_start(list, format);
+
+  if (format == NULL)
+    return (-1);
+
+  for (i = 0; format[i] != '\0'; i++)
+    {
+      if (format[i] != '%')
+        {
+          buffer[buff_ind++] = format[i];
+          if (buff_ind == BUFF_SIZE)
+            {
+              print_buffer(buffer, &buff_ind);
+              print_char += buff_ind;
+            }
+          else
+            {
+              print_char++;
+            }
+        }
+      else
+        {
+          print_buffer(buffer, &buff_ind);
+          i++;
+          switch (format[i])
+		     {
+            case 'c':
+              write(1, &va_arg(list, int), 1);
+              print_char++;
+              break;
+            case 's':
+              string = va_arg(list, char *);
+              if (string == NULL)
+                string = "(null)";
+              while (*string != '\0')
+                {
+                  write(1, string, 1);
+                  string++;
+                  print_char++;
+                }
+              break;
+            case '%':
+              write(1, &format[i], 1);
+              print_char++;
+              break;
+            case 'b':
+              n = va_arg(list, unsigned int);
+              print_binary(n, &buff_ind, buffer);
+              print_char += buff_ind;
+			                break;
+            default:
+              write(1, "%", 1);
+              print_char++;
+              if (format[i] != '\0')
+                {
+                  write(1, &format[i], 1);
+                  print_char++;
+                }
+              break;
+            }
+        }
+    }
+  print_buffer(buffer, &buff_ind);
+  va_end(list);
+  return (print_char);
+}
