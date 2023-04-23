1b2efb7bb10eb1cc0e2df9a0965b2abb466aafe1
EzechielKiregha <ekiregha@gmail.com>
2023-04-22 18:33:55 -0700

    With a face like mine, I do better in print

--- 0x11-C_printf/0-print_character_string.c
+++ 0x11-C_printf/0-print_character_string.c
@@ -1,7 +1,14 @@
 #include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
-
+/**
+ * handle_binary - Converts an unsigned integer to binary and prints it
+ * @args: A va_list of arguments passed to the calling function
+ *
+ * Return: The total number of characters printed,
+ * including the converted number
+ */
+int handle_binary(va_list args);
 /**
  * _printf - produces output according to a format
  * @format: character string containing format specifier
@@ -10,50 +17,80 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int printed_chars = 0;
+	int i = 0, printed_chars = 0, len = 0;
+	va_list args;
 
-    va_start(args, format);
+	va_start(args, format);
+	for (; (*format); format++)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			printed_chars++;
+		}
+		else
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					{
+						char c = 'h';
 
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;	    
-            if (*format == 'c')
-            {
-                char c = va_arg(args, int);
-                write(1, &c, 1);
-                printed_chars++;
-            }
-            else if (*format == 's')
-            {
-                char *s = va_arg(args, char *);
-                if (s)
-                {
-                    while (*s)
-                    {
-                        write(1, s, 1);
-                        s++;
-                        printed_chars++;
-                    }
-                }
-            }
-            else if (*format == '%')
-	      {
-                write(1, "%", 1);
-                printed_chars++;
-            }
-        }
-        else
-        {
-            write(1, format, 1);
-            printed_chars++;
-        }
-        format++;
-    }
+						if(c)
+						       c = va_arg(args, int);
+						printed_chars++;
+						break;
+					}
+				case 's':
+				{
+					char *s = va_arg(args, char*);
 
-    va_end(args);
+					for (; s[len]; i++)
+					{
+						len++;
+					}
+					write(1, s, len);
+					printed_chars += len;
+					break;
+				}
+				case '%':
+					{
+						write(1, "%", 1);
+						printed_chars++;
+						break;
+					}
+				case 'b':
+					{
+						printed_chars = handle_binary(args);
+						break;
+					}
+			}
+		}
+	}
+	va_end(args);
+	return (printed_chars);
+}
+/**
+ * handle_binary - Converts an unsigned integer to binary and prints it
+ * @args: A va_list of arguments passed to the calling function
+ * Return: The total number of characters printed,
+ * including the converted number
+ */
+int handle_binary(va_list args)
+{
+	char buffer[33];
+	unsigned int num;
+	int i;
 
-    return (printed_chars);
+	num = va_arg(args, unsigned int);
+	i = 0;
+	while (num)
+	{
+		buffer[i++] = (num % 2) + '0';
+		num >>= 1;
+	}
+	while (i--)
+		write(1, &buffer[i], 1);
+	return (i);
 }
--- 0x11-C_printf/main.h
+++ 0x11-C_printf/main.h
@@ -1,10 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
 
+int handle_binary(va_list args);
 int _printf(const char *format, ...);
-int _putchar(char c);
 
 #endif
